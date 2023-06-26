1e4a9bb885b6013ba456aa74bd605707816a2f23
Felipe Aravena <hernan.aravena.k@usach.cl>
2023-06-26 00:21:36 -0400

    [Update] Se agrega el config service

--- acopio-service/src/main/java/cl/usach/mingeso/acopioservice/AcopioServiceApplication.java
+++ acopio-service/src/main/java/cl/usach/mingeso/acopioservice/AcopioServiceApplication.java
@@ -2,8 +2,10 @@ package cl.usach.mingeso.acopioservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AcopioServiceApplication {
 
     public static void main(String[] args) {
---
+++ config-server/src/main/java/cl/usach/mingeso/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package cl.usach.mingeso.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
