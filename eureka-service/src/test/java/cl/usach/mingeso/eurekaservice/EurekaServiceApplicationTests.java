37323754b3b659910b70ae1313c9d34c36665207
Felipe Aravena <hernan.aravena.k@usach.cl>
2023-06-26 01:07:54 -0400

    [Feature] Se agrega el eureka-service

---
+++ eureka-service/src/test/java/cl/usach/mingeso/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.mingeso.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
