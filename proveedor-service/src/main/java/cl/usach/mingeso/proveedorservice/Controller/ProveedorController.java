c7d0535f2133e4a4bb7dba4bc6e9c8710de4e4e8
Felipe Aravena <hernan.aravena.k@usach.cl>
2023-06-25 23:32:41 -0400

    [Update] Se agregan los controladores necesarios para crear un proveedor y para eliminar a un proveedor

--- proveedor-service/src/main/java/cl/usach/mingeso/proveedorservice/Controller/ProveedorController.java
+++ proveedor-service/src/main/java/cl/usach/mingeso/proveedorservice/Controller/ProveedorController.java
@@ -17,5 +17,14 @@ public class ProveedorController {
     @GetMapping("/listar-proveedores")
     public List<ProveedorEntity> obtenerProveedores(){ return proveedorService.obtenerProveedores(); }
 
+    @PostMapping("/crear-proveedor")
+    public void crearProveedor(@RequestBody ProveedorEntity proveedor) {
+        proveedorService.guardarProveedor(proveedor.getCodigo(), proveedor.getNombre(), proveedor.getCategoria(), proveedor.getRetencion());
+    }
+
+    @DeleteMapping("/eliminar-proveedor/{codigo}")
+    public void eliminarProveedor(@PathVariable String codigo) {
+        proveedorService.eliminarProveedor(codigo);
+    }
 
 }
